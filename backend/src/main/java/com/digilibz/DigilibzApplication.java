144e2a0d2123405ddb1ce501bbb8a52f8757cb59
Arfazrll <azril4974@gmail.com>
2025-12-09 17:10:57 +0700

    SetupApp

---
+++ backend/src/main/java/com/digilibz/DigilibzApplication.java
@@ -0,0 +1,13 @@
+package com.digilibz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigilibzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigilibzApplication.class, args);
+    }
+
+}
